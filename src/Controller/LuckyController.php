0405c03a6d50862f2665fb3438ea698c56ecc6da
Ryan C. Durham <ryan@stagerightlabs.com>
2021-12-23 16:48:44 -0800

    Set up the 'lucky' controller

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/lucky', name: 'lucky')]
+    public function index(LoggerInterface $logger): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            "<html><body>Lucky number: {$number}</body></html>"
+        );
+
+        // return $this->render('lucky/index.html.twig', [
+        //     'controller_name' => 'LuckyController',
+        // ]);
+    }
+}
