7f753f56f210b5f8dc2e08dd8a14d55781b4e05b
Ryan C. Durham <ryan@stagerightlabs.com>
2021-12-27 11:11:16 -0800

    Apply code formatting

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LuckyController extends AbstractController
 {
