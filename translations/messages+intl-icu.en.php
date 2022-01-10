643ea54d3a813423483b2ed3427496fcf0ef3ece
Ryan C. Durham <ryan@stagerightlabs.com>
2022-01-10 15:56:52 -0800

    Basic css styling with Tailwind

--- translations/messages+intl-icu.en.php
+++ translations/messages+intl-icu.en.php
@@ -1,8 +1,23 @@
 <?php
 
 return [
+    'forms' => [
+        'register' => [
+            'email' => 'Email',
+            'password' => 'Password',
+            'register' => 'Register',
+            'title' => 'Create Your Account',
+        ],
+        'login' => [
+            'email' => 'Email',
+            'password' => 'Password',
+            'remember_me' => 'Remember Me',
+            'title' => 'Please Sign In',
+            'sign_in' => 'Sign In',
+        ]
+    ],
     'lucky' => [
         'title' => 'Lucky Number',
         'number' => 'Your lucky number is {number}',
-    ]
+    ],
 ];
