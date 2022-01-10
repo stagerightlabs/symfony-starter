bdf3a6c07808e0a5c4f9a47a7fd6ccc63718b56b
Ryan C. Durham <ryan@stagerightlabs.com>
2022-01-10 15:59:28 -0800

    Set up user login and registration

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Action\CreateUserAction;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/{_locale}/register', name: 'register')]
+    public function register(Request $request, CreateUserAction $action): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $action = $action->run([
+                'email' => $form->get('email')->getData(),
+                'password' => $form->get('plainPassword')->getData()
+            ]);
+
+            if ($action->failed()) {
+                $this->addFlash('error', $action->getMessage());
+                return $this->redirectToRoute('register');
+            }
+
+            return $this->redirectToRoute('lucky');
+        }
+
+        return $this->render('auth/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
